663744226a0f24d0039ba4ac54d640e6ff0b32a5
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-17 15:18:09 +0100

    Install Grafana with nginx.
    - Install and configure Grafana with nginx.
    - Modify the default root password in InfluxDB.
    - Add a new DB, user and password to store analytics in InfluxDB.
    - Add a new DB to be used by Grafana.
    
    Change-Id: I26a82ba6c9e1efcfb3ad46cdf581693d0c66538a

---
+++ deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/init.pp
@@ -0,0 +1,66 @@
+# == Class: lma_monitoring_analytics
+
+class lma_monitoring_analytics (
+  $influxdb_host     = $lma_monitoring_analytics::params::influxdb_host,
+  $influxdb_dbname   = undef,
+  $influxdb_username = undef,
+  $influxdb_userpass = undef,
+  $influxdb_rootpass = undef,
+) inherits lma_monitoring_analytics::params {
+
+  $grafana_dir        = $lma_monitoring_analytics::params::grafana_dir
+  $grafana_conf       = $lma_monitoring_analytics::params::grafana_conf
+  $grafana_dash       = $lma_monitoring_analytics::params::grafana_dash
+  $configure_influxdb = $lma_monitoring_analytics::params::influxdb_script
+  $influxdb_grafanadb = $lma_monitoring_analytics::params::influxdb_grafanadb
+
+  # Configure InfluxDB:
+  #   - update root password
+  #   - create the user and db for metrics
+  #   - create the db for grafana
+
+  file { $configure_influxdb:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0744',
+    content => template('lma_monitoring_analytics/configure_influxdb.sh.erb'),
+    notify  => Exec['configure_influxdb_script']
+  }
+
+  exec { 'configure_influxdb_script':
+    command => $configure_influxdb,
+  }
+
+  # Deploy sources.
+  file { $grafana_dir:
+    source  => 'puppet:///modules/lma_monitoring_analytics/grafana/sources',
+    recurse => true,
+  }
+
+  # Replace config.js
+  file { $grafana_conf:
+    ensure  => file,
+    content => template('lma_monitoring_analytics/grafana/config.js.erb'),
+    require => File[$grafana_dir],
+  }
+
+  # Install the dashboard
+  file { $grafana_dash:
+    source  => 'puppet:///modules/lma_monitoring_analytics/grafana/dashboards/lma.json',
+    require => File[$grafana_dir],
+  }
+
+  # And now install nginx
+  class { 'nginx':
+    manage_repo           => false,
+    nginx_vhosts          => {
+      'grafana.local' => {
+        'www_root' => $grafana_dir
+      }
+    },
+    nginx_vhosts_defaults => {
+      'listen_options' => 'default_server'
+    },
+    require               => File[$grafana_conf],
+  }
+}
