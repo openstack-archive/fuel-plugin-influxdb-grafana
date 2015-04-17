68e38a903ab446f672e1a798d9fab773d09536d4
Simon Pasquier <spasquier@mirantis.com>
2015-04-17 15:12:21 +0200

    Upload Grafana dashboards to InfluxDB
    The Grafana dashboards that were located in the contrib/ directory are
    now uploaded by Puppet to the InfluxDB database. This change also
    refactors the existing code to split the configuration of InfluxDB and
    Grafana into separate manifests.
    
    Change-Id: I3df69ed84f754d36f0206a2d3b3d1238e791e398

--- deployment_scripts/puppet/manifests/influxdb.pp
+++ deployment_scripts/puppet/manifests/influxdb.pp
@@ -2,7 +2,10 @@ $influxdb_grafana = hiera('influxdb_grafana')
 
 if $influxdb_grafana['node_name'] == hiera('user_node_name') {
 
-  class { 'influxdb':
-    install_from_repository => true,
+  class { 'lma_monitoring_analytics::influxdb':
+    influxdb_rootpass => $influxdb_grafana['influxdb_rootpass'],
+    influxdb_dbname   => $influxdb_grafana['influxdb_dbname'],
+    influxdb_username => $influxdb_grafana['influxdb_username'],
+    influxdb_userpass => $influxdb_grafana['influxdb_userpass'],
   }
 }
