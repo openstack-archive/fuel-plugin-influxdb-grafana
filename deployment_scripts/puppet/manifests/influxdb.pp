eefc7138627154be62526f44fffa51cb6c0e5c58
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-04-18 11:29:18 +0200

    Manage the size of the partition
    Now we can choose the size of the partition. Currently you can only use
    three disks and the size is in GB. If the size of the disk is not
    provided we use all the remaining free space available on the disk. If
    you are asking for a size that doesn't fit, installation will fail.
    
    Change-Id: I3db2ad4f2fff874bcacccb3a27df2686d30ac9b1

--- deployment_scripts/puppet/manifests/influxdb.pp
+++ deployment_scripts/puppet/manifests/influxdb.pp
@@ -4,5 +4,6 @@ if $influxdb_grafana['node_name'] == hiera('user_node_name') {
 
   class { 'influxdb':
     install_from_repository => true,
+    storage_dir             => $influxdb_grafana['data_dir'],
   }
 }
