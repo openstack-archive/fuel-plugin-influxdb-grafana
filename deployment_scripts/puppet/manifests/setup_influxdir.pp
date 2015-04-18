8230cb7aa8460c6d1c1c5be4edcfc126c0a86ee5
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-04-18 11:29:18 +0200

    Manage the size of the partition
    Now we can choose the size of the partition. Currently you can only use
    three disks and the size is in GB. If the size of the disk is not
    provided we use all the remaining free space available on the disk. If
    you are asking for a size that doesn't fit, installation will fail.
    
    Currently we create a volume and we mount it on /opt/influxdb.
    
    Change-Id: I3db2ad4f2fff874bcacccb3a27df2686d30ac9b1

---
+++ deployment_scripts/puppet/manifests/setup_influxdir.pp
@@ -0,0 +1,33 @@
+$influxdb_grafana = hiera('influxdb_grafana')
+
+if $influxdb_grafana['node_name'] == hiera('user_node_name') {
+
+  $directory = $influxdb_grafana['data_dir']
+  $disks = split($::unallocated_pvs, ',')
+
+  validate_array($disks)
+
+  user { 'influxdb':
+    ensure => present,
+    system => true,
+    shell  => '/sbin/nologin',
+  }
+
+  if empty($disks) {
+    file { $directory:
+      ensure  => 'directory',
+      owner   => 'influxdb',
+      group   => 'influxdb',
+      require => User['influxdb'],
+    }
+  } else {
+    disk_management::lvm_fs { $directory:
+      owner   => 'influxdb',
+      group   => 'influxdb',
+      disks   => $disks,
+      lv_name => 'influxdb-data',
+      vg_name => 'influxdb',
+      require => User['influxdb'],
+    }
+  }
+}
