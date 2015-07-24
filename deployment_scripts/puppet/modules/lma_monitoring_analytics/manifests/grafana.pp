d1c38a44469581e978480b2e58b33e1176b811bb
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-07-24 11:54:20 +0200

    [WIP] Upgrade Grafana to 2.1
    It downloads nightly build and uses the Puppet module of "bfraser"
    to install and configure Grafana.
    It also adds two new parameters in the UI that are the username and the
    password of the Grafana admin user.
    
    Blueprint: upgrade-influxdb-grafana
    Change-Id: Ibb6fc7bb523800d6a14ed659d2fa02b726742984

--- deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/grafana.pp
+++ deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/grafana.pp
@@ -15,102 +15,21 @@
 # == Class: lma_monitoring_analytics::grafana
 
 class lma_monitoring_analytics::grafana (
-  $listen_port       = $lma_monitoring_analytics::params::listen_port,
-  $influxdb_dbname   = undef,
-  $influxdb_username = undef,
-  $influxdb_userpass = undef,
+    $admin_username = undef,
+    $admin_password = undef,
+    $http_port      = $lma_monitoring_analytics::params::listen_port,
 ) inherits lma_monitoring_analytics::params {
-
-  $grafana_dir        = $lma_monitoring_analytics::params::grafana_dir
-  $grafana_conf       = $lma_monitoring_analytics::params::grafana_conf
-  $influxdb_host      = $lma_monitoring_analytics::params::influxdb_host
-  $grafana_dbname     = $lma_monitoring_analytics::params::grafana_dbname
-  $grafana_home_dashboard = $lma_monitoring_analytics::params::grafana_home_dashboard
-
-  # Deploy sources
-  file { $grafana_dir:
-    source  => 'puppet:///modules/lma_monitoring_analytics/grafana/sources',
-    recurse => true,
-  }
-
-  # Replace config.js
-  file { $grafana_conf:
-    ensure  => file,
-    content => template('lma_monitoring_analytics/grafana/config.js.erb'),
-    require => File[$grafana_dir],
-  }
-
-  # Install the dashboards
-  $dashboard_defaults = {
-    ensure           => present,
-    storage_url      => "http://localhost:8086/db/${grafana_dbname}",
-    storage_user     => $influxdb_username,
-    storage_password => $influxdb_userpass,
-  }
-  $dashboards = {
-    'Main' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Main.json'),
+  class { '::grafana':
+    install_method      => 'repo',
+    manage_package_repo => false,
+    cfg                 => {
+      server   => {
+        http_port => $http_port,
+      },
+      security => {
+        admin_user     => $admin_username,
+        admin_password => $admin_password,
+      },
     },
-    'Apache' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Apache.json'),
-    },
-    'Ceph' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Ceph.json'),
-    },
-    'Ceph OSD' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Ceph_OSD.json'),
-    },
-    'Cinder' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Cinder.json'),
-    },
-    'Glance' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Glance.json'),
-    },
-    'HAProxy' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/HAProxy.json'),
-    },
-    'Keystone' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Keystone.json'),
-    },
-    'LMA self-monitoring' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/LMA.json'),
-    },
-    'Memcached' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Memcached.json'),
-    },
-    'MySQL' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/MySQL.json'),
-    },
-    'Neutron' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Neutron.json'),
-    },
-    'Nova' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Nova.json'),
-    },
-    'RabbitMQ' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/RabbitMQ.json'),
-    },
-    'Heat' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/Heat.json'),
-    },
-    'System' => {
-      content => template('lma_monitoring_analytics/grafana_dashboards/System.json'),
-    },
-  }
-  create_resources(grafana_dashboard, $dashboards, $dashboard_defaults)
-
-  # And now install nginx
-  class { 'nginx':
-    manage_repo           => false,
-    nginx_vhosts          => {
-      'grafana.local' => {
-        'www_root' => $grafana_dir
-      }
-    },
-    nginx_vhosts_defaults => {
-      'listen_port'    => $listen_port,
-      'listen_options' => 'default_server'
-    },
-    require               => File[$grafana_conf],
   }
 }
