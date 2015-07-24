53d10778aeb5c3df1233e898e0538b3b3fcd78dd
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-07-24 11:54:20 +0200

    [WIP] Upgrade Grafana to 2.1
    It downloads nightly build and uses the Puppet module of "bfraser"
    to install and configure Grafana.
    It also adds two new parameters in the UI that are the username and the
    password of the Grafana user.
    
    Blueprint: [LMA] Upgrade InfluxDB and Grafana components
    Change-Id: Ibb6fc7bb523800d6a14ed659d2fa02b726742984

--- deployment_scripts/puppet/manifests/grafana.pp
+++ deployment_scripts/puppet/manifests/grafana.pp
@@ -13,27 +13,11 @@
 #    under the License.
 #
 $influxdb_grafana = hiera('influxdb_grafana')
-$elasticsearch_kibana = hiera('elasticsearch_kibana', undef)
 $user_node_name = hiera('user_node_name')
 
 if $influxdb_grafana['node_name'] == $user_node_name {
-
-  # We want to be able to deploy this plugin with Elasticsearch-Kibana. So
-  # we check if EK plugin is deployed and if it is the case we set the listen
-  # port to 8000 instead of 80.
-  if $elasticsearch_kibana {
-    $listen_port = $elasticsearch_kibana['node_name'] ? {
-      $user_node_name => 8000,
-      default         => 80,
-    }
-  } else {
-    $listen_port = 80
-  }
-
   class {'lma_monitoring_analytics::grafana':
-    listen_port       => $listen_port,
-    influxdb_dbname   => $influxdb_grafana['influxdb_dbname'],
-    influxdb_username => $influxdb_grafana['influxdb_username'],
-    influxdb_userpass => $influxdb_grafana['influxdb_userpass'],
+    db_username => $influxdb_grafana['grafana_username'],
+    db_password => $influxdb_grafana['grafana_userpass'],
   }
 }
