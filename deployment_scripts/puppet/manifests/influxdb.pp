3da785529cde89055055177973355bad3ee9d23b
Simon Pasquier <spasquier@mirantis.com>
2015-08-07 11:37:13 +0200

    Add support for retention policy
    This changes adds the support for configuring the retention policy in
    InfluxDB. Only the retention period is exposed to the end-user, the
    replication factor will be exposed once the plugin supports clustering
    with multiple nodes.
    
    Change-Id: I365ece96fabc72bb55feb0969c1c452a8febc895

--- deployment_scripts/puppet/manifests/influxdb.pp
+++ deployment_scripts/puppet/manifests/influxdb.pp
@@ -15,12 +15,20 @@
 $influxdb_grafana = hiera('influxdb_grafana')
 
 if $influxdb_grafana['node_name'] == hiera('user_node_name') {
+  # retention period value is expressd in days
+  if $influxdb_grafana['retention_period'] == 0 {
+    $retention_period = 'INF'
+  } else {
+    $retention_period = sprintf('%dd', $influxdb_grafana['retention_period'])
+  }
 
   class { 'lma_monitoring_analytics::influxdb':
-    influxdb_rootpass => $influxdb_grafana['influxdb_rootpass'],
-    influxdb_dbname   => $influxdb_grafana['influxdb_dbname'],
-    influxdb_username => $influxdb_grafana['influxdb_username'],
-    influxdb_userpass => $influxdb_grafana['influxdb_userpass'],
-    influxdb_dir      => $influxdb_grafana['data_dir'],
+    influxdb_rootpass  => $influxdb_grafana['influxdb_rootpass'],
+    influxdb_dbname    => $influxdb_grafana['influxdb_dbname'],
+    influxdb_username  => $influxdb_grafana['influxdb_username'],
+    influxdb_userpass  => $influxdb_grafana['influxdb_userpass'],
+    influxdb_dir       => $influxdb_grafana['data_dir'],
+    retention_period   => $retention_period,
+    replication_factor => $influxdb_grafana['replication_factor'],
   }
 }
