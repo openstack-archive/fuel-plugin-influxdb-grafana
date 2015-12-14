39bcdcca358c29f6b3b1743de9f9f604702010c7
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-12-14 08:53:58 +0100

    Install InfluxDB in HA mode
    - Allow to deploy three influxdb_grafana nodes at most
    - Add firewall rule to allow traffic between the nodes of the raft cluster
    - Create a VIP
    - Create a file /etc/hiera/override/influxdb_grafana.yaml that contains
      information about nodes that belong to the raft cluster and the VIP
    - Install and configure HAProxy
    
    Implements: blueprint influxdb-clustering
    Change-Id: I0852da40d709563adc8b89bbad240ae89dfa862e

---
+++ deployment_scripts/puppet/manifests/influxdb-haproxy.pp
@@ -0,0 +1,34 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+$influxdb_port = '8086'
+$influxdb_nodes = hiera(lma::influxdb::raft_nodes)
+
+openstack::ha::haproxy_service { 'influxdb':
+  order                  => '800',
+  listen_port            => $influxdb_port,
+  balancermember_port    => $influxdb_port,
+  haproxy_config_options => {
+    'option'  => ['httplog'],
+    'balance' => 'roundrobin',
+    'mode'    => 'http',
+  },
+  balancermember_options => "check port ${influxdb_port}",
+  internal               => true,
+  internal_virtual_ip    => hiera(lma::influxdb::vip),
+  public                 => false,
+  public_virtual_ip      => undef,
+  ipaddresses            => values($influxdb_nodes),
+  server_names           => keys($influxdb_nodes),
+}
