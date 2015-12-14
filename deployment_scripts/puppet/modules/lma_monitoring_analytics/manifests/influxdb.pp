1619360a83b0471cdd2e6a05b58fad9cab6dba64
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-12-14 08:53:58 +0100

    Install InfluxDB in HA mode
    - Allow to deploy three influxdb_grafana nodes at most
    - Add firewall rule to allow traffic between the nodes of the raft
      cluster
    - Create a VIP
    - Create a file /etc/hiera/override/influxdb_grafana.yaml that contains
      information about nodes that belong to the raft cluster and the VIP
    - Install and configure HAProxy
    
    Implements: blueprint influxdb-clustering
    Change-Id: I0852da40d709563adc8b89bbad240ae89dfa862e

--- deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/influxdb.pp
+++ deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/influxdb.pp
@@ -22,6 +22,8 @@ class lma_monitoring_analytics::influxdb (
   $influxdb_dir       = $lma_monitoring_analytics::params::influxdb_dir,
   $retention_period   = $lma_monitoring_analytics::params::influxdb_retention_period,
   $replication_factor = $lma_monitoring_analytics::params::influxdb_replication_factor,
+  $raft_hostname      = undef,
+  $raft_nodes         = undef,
 ) inherits lma_monitoring_analytics::params {
 
   $configure_influxdb = $lma_monitoring_analytics::params::influxdb_script
@@ -31,11 +33,15 @@ class lma_monitoring_analytics::influxdb (
     $real_retention_period = $retention_period
   }
 
+  validate_array($raft_nodes)
+
   class { '::influxdb':
-    data_dir => "${influxdb_dir}/data",
-    meta_dir => "${influxdb_dir}/meta",
-    hh_dir   => "${influxdb_dir}/hh",
-    wal_dir  => "${influxdb_dir}/wal",
+    data_dir      => "${influxdb_dir}/data",
+    meta_dir      => "${influxdb_dir}/meta",
+    hh_dir        => "${influxdb_dir}/hh",
+    wal_dir       => "${influxdb_dir}/wal",
+    raft_hostname => $raft_hostname,
+    raft_nodes    => $raft_nodes,
   }
 
   file { $configure_influxdb:
