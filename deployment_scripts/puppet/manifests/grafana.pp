3e69a3e2345bc56e187c8d3c9bc7283e1194257d
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-12-28 10:10:42 +0100

    Install Grafana with a MySQL database
    Add an option to use a the MySQL cluster that is deployed locally with
    your environment or to select the hostname or the IP address of a MySQL
    cluster that is deployed and configured elsewhere.
    
    Change-Id: I25aaae3160ae2344454ad4585660a1ee3cfffb71
    Implements: blueprint influxdb-clustering

--- deployment_scripts/puppet/manifests/grafana.pp
+++ deployment_scripts/puppet/manifests/grafana.pp
@@ -16,11 +16,66 @@
 prepare_network_config(hiera('network_scheme', {}))
 $mgmt_address = get_network_role_property('management', 'ipaddr')
 $influxdb_grafana = hiera('influxdb_grafana')
-class {'lma_monitoring_analytics::grafana':
-  admin_username    => $influxdb_grafana['grafana_username'],
-  admin_password    => $influxdb_grafana['grafana_userpass'],
-  influxdb_username => $influxdb_grafana['influxdb_username'],
-  influxdb_password => $influxdb_grafana['influxdb_userpass'],
-  influxdb_database => $influxdb_grafana['influxdb_dbname'],
-  domain            => $mgmt_address,
+$db_mode = $influxdb_grafana['grafana_db']
+
+case $db_mode {
+
+  'local': {
+    $mysql  = hiera('mysql')
+
+    $db_vip = hiera(database_vip)
+    $db_name = 'grafana'
+    $db_user = 'grafana'
+    $db_pass = 'grafana'
+    $db_admin_user = 'root'
+    $db_admin_pass = $mysql['root_password']
+    $db_port = '3306'
+    $db_options_file = '/root/.my.cnf'
+    $db_file_content = inline_template('[client]
+user=<%= @db_admin_user %>
+password=<%= @db_admin_pass %>
+host=<%= @db_vip %>
+')
+
+    file { $db_options_file:
+      ensure  => file,
+      content => $db_file_content,
+    } ->
+    mysql::db { $db_name:
+      user     => $db_user,
+      password => $db_pass,
+      host     => $db_vip,
+    } ->
+    class {'lma_monitoring_analytics::grafana':
+      db_host           => "${db_vip}:${db_port}",
+      db_user           => $db_user,
+      db_password       => $db_pass,
+      admin_username    => $influxdb_grafana['grafana_username'],
+      admin_password    => $influxdb_grafana['grafana_userpass'],
+      influxdb_username => $influxdb_grafana['influxdb_username'],
+      influxdb_password => $influxdb_grafana['influxdb_userpass'],
+      influxdb_database => $influxdb_grafana['influxdb_dbname'],
+      domain            => $mgmt_address,
+    }
+  }
+
+  'remote': {
+    # In this case we suppose that a DB named 'grafana' has been created.
+    class {'lma_monitoring_analytics::grafana':
+      db_host           => $influxdb_grafana['grafana_dbhost'],
+      db_user           => $influxdb_grafana['grafana_dbuser'],
+      db_password       => $influxdb_grafana['grafana_dbpassword'],
+      admin_username    => $influxdb_grafana['grafana_username'],
+      admin_password    => $influxdb_grafana['grafana_userpass'],
+      influxdb_username => $influxdb_grafana['influxdb_username'],
+      influxdb_password => $influxdb_grafana['influxdb_userpass'],
+      influxdb_database => $influxdb_grafana['influxdb_dbname'],
+      domain            => $mgmt_address,
+    }
+  }
+
+  default: {
+    fail("'${db_mode}']}' database not supported for Grafana")
+  }
 }
+
