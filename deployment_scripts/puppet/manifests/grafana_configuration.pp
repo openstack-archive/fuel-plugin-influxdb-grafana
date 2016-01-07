8bf667b4ae97eff31e448fa6ddfd761d0de2052e
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-01-07 15:18:54 +0100

    Configure Grafana only once
    This change adds grafana dashboards and datasource only on
    the primary node.
    
    Implements: blueprint influxdb-clustering
    Change-Id: I5eb93ee01ab8fc6e4bb6d3ad88299293ac1cd85d

---
+++ deployment_scripts/puppet/manifests/grafana_configuration.pp
@@ -0,0 +1,41 @@
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+$mgmt_vip = hiera('lma::influxdb::vip')
+$influxdb_grafana = hiera('influxdb_grafana')
+
+$admin_username = $influxdb_grafana['grafana_username']
+$admin_password = $influxdb_grafana['grafana_userpass']
+$influxdb_username = $influxdb_grafana['influxdb_username']
+$influxdb_password = $influxdb_grafana['influxdb_userpass']
+$influxdb_database = $influxdb_grafana['influxdb_dbname']
+
+grafana_datasource { 'lma':
+  ensure           => present,
+  url              => "http://${mgmt_vip}:8086",
+  user             => $influxdb_username,
+  password         => $influxdb_password,
+  database         => $influxdb_database,
+  access_mode      => 'proxy',
+  is_default       => true,
+  grafana_url      => "http://${mgmt_vip}:8000",
+  grafana_user     => $admin_username,
+  grafana_password => $admin_password,
+}
+
+class {'lma_monitoring_analytics::grafana_dashboards':
+  admin_username => $admin_username,
+  admin_password => $admin_password,
+  host           => $mgmt_vip,
+}
