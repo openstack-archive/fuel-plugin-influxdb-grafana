29d84f38c75be068e30595b72a560259e928eb1f
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-01-07 09:59:34 +0100

    Separate InfluxDB administration from configuration
    This change adds a class to be able to create an admin user and it also
    creates a defined type to create database and its associated user.
    This allows the creation of the LMA database when InfluxDB cluster is
    up and ready.
    
    Implements: blueprint influxdb-clustering
    Change-Id: I5e40ff510385f867d26b77a2af5ed278a0f9cc15

--- deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/params.pp
+++ deployment_scripts/puppet/modules/lma_monitoring_analytics/manifests/params.pp
@@ -17,9 +17,8 @@
 class lma_monitoring_analytics::params {
   $listen_port                 = 8000
   $influxdb_url                = 'http://localhost:8086'
-  $influxdb_script             = '/usr/local/bin/configure_influxdb.sh'
   $influxdb_dir                = '/var/lib/influxdb'
-  $influxdb_retention_period   = 0
+  $influxdb_retention_period   = 'INF'
   $influxdb_replication_factor = 1
   $grafana_domain              = 'localhost'
 }
