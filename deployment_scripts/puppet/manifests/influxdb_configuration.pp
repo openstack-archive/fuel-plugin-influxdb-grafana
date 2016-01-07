cc258cf66e9232e232387f2dd1f54804a7869712
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-01-07 09:59:34 +0100

    Separate InfluxDB administration from configuration
    This change adds a class to be able to create an admin user and it also
    creates a defined type to create database and its associated user.
    This allows the creation of the LMA database when InfluxDB cluster is
    up and ready.
    
    Implements: blueprint influxdb-clustering
    Change-Id: I5e40ff510385f867d26b77a2af5ed278a0f9cc15

---
+++ deployment_scripts/puppet/manifests/influxdb_configuration.pp
@@ -0,0 +1,46 @@
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+$influxdb_grafana = hiera('influxdb_grafana')
+$vip = hiera('lma::influxdb::vip')
+
+$admin_user = 'root'
+$admin_password = $influxdb_grafana['influxdb_rootpass']
+$username = $influxdb_grafana['influxdb_username']
+$password = $influxdb_grafana['influxdb_userpass']
+$retention_period = $influxdb_grafana['retention_period']
+
+lma_monitoring_analytics::influxdb_user { $admin_user:
+  password     => $admin_password,
+  admin_role   => true,
+  influxdb_url => "http://${vip}:8086",
+}
+
+lma_monitoring_analytics::influxdb_user { $username:
+  admin_user     => $admin_user,
+  admin_password => $admin_password,
+  password       => $password,
+  influxdb_url   => "http://${vip}:8086",
+  require        => Lma_monitoring_analytics::Influxdb_user[$admin_user],
+}
+
+lma_monitoring_analytics::influxdb_database { 'lma':
+  admin_user       => $admin_user,
+  admin_password   => $admin_password,
+  influxdb_url     => "http://${vip}:8086",
+  db_user          => $username,
+  db_password      => $password,
+  retention_period => $retention_period,
+  require          => Lma_monitoring_analytics::Influxdb_user[$username],
+}
