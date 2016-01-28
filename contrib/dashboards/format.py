6ce291b56e1b5cbe7379596f6174d4c449361a5e
Swann Croiset <scroiset@mirantis.com>
2016-01-28 14:11:44 +0100

    Reformat dashboards to facilitate comparisons and updates
    It's always painful to understand which modifications are made on
    dashboards.
    
    The patch includes a Python script to automate these operations:
    * reformat JSON files by ordering the keys
    * remove template variables
    * override the time picker to 'last 1 hour'
    * increment version
    
    Change-Id: I489e6d5019a4ca8c15c0ff72c8d17585f169df96

---
+++ contrib/dashboards/format.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import sys
+import glob
+import os
+import simplejson as json
+
+def usage():
+    h = """
+    Format JSON file with ordered keys.
+    Remove sections:
+        templating.list[].current
+        templating.list[].options
+    Override time entry to {"from": "now-1h","to": "now"}
+    Increment the version
+
+    WARNING: this script modifies all manipulated files.
+
+    Usage: {} <DIRECTORY|FILE>
+
+    if a DIRECTORY is provided, all files with suffix '.json' will be modified.
+    """
+    print h.format(sys.argv[0])
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(1)
+
+arg = sys.argv[1]
+
+if os.path.isdir(arg):
+    path = "{}/*.json".format(arg)
+elif os.path.isfile(arg):
+    path = arg
+else:
+    print "'{}' no such file or directory".format(arg)
+    usage()
+    sys.exit(1)
+
+for f in glob.glob(path):
+    data = None
+    absf = os.path.abspath(f)
+    with open(absf) as out:
+        data = json.load(out)
+    for k, v in data.iteritems():
+        if k == 'templating':
+            variables = v.get('list', [])
+            for o in variables:
+                o['options'] = []
+                o['current'] = {}
+    data['time'] = { 'from':'now-1h', 'to':'now'}
+
+    if not data.get('version', None):
+        data['version'] = 1
+    else:
+        data['version'] = int(data['version']) + 1
+
+    with open(absf, 'w') as out:
+        out.write(json.dumps(data, indent=2, sort_keys=True))
