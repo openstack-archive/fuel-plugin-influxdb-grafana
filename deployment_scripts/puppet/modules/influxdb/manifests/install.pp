e04268fa794c4d08f1d1ebd7d6d0b90822908667
Swann Croiset <scroiset@mirantis.com>
2016-03-25 12:04:12 +0100

    Fix wrong permission of logrotate configuration file
    Fixes-Bug: #1535440
    Change-Id: Ic0d1755932a6f5380dac94d7d99d805788779759

--- deployment_scripts/puppet/modules/influxdb/manifests/install.pp
+++ deployment_scripts/puppet/modules/influxdb/manifests/install.pp
@@ -36,4 +36,10 @@ class influxdb::install {
   file { '/etc/logrotate.d/influxdb':
     ensure => absent,
   }
+  # Fix the wrong permission set by the influxdb package
+  file { '/etc/logrotate.d/influxd':
+    ensure  => present,
+    mode    => '644',
+    require => Package['influxdb'],
+  }
 }
