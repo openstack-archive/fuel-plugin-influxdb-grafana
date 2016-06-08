f48aa7cc43614d6275ff69aa68050a5893ae852e
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-06-08 10:21:27 +0200

    [WIP] Enable secure communication over HTTPS
    This patch configures HAProxy to terminate SSL connection when the
    support for secure communications is enabled in StackLight.
    
    Change-Id: Idf59f92f00ed58f3ebce2e0421694ad7c2010fe9
    Implements: blueprint support-secure-communication
    Depends-On: Ic3f730b0b0f950aeb738106a17c2e0bca62431d2

--- deployment_scripts/puppet/modules/lma_monitoring_analytics/lib/puppet/provider/grafana.rb
+++ deployment_scripts/puppet/modules/lma_monitoring_analytics/lib/puppet/provider/grafana.rb
@@ -15,6 +15,7 @@
 require 'cgi'
 require 'json'
 require 'net/http'
+require 'net/https'
 
 class Puppet::Provider::Grafana < Puppet::Provider
     # Helper methods
@@ -78,9 +79,11 @@ class Puppet::Provider::Grafana < Puppet::Provider
             request.basic_auth resource[:grafana_user], resource[:grafana_password]
         end
 
-        return Net::HTTP.start(self.grafana_host, self.grafana_port) do |http|
-            http.request(request)
-        end
+        return Net::HTTP.start(self.grafana_host, self.grafana_port,
+                               :use_ssl => self.grafana_scheme == "https",
+                               :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+                                   http.request(request)
+                               end
     end
 end
 
