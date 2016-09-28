296e949209252cae8e7d7a220ab923d204ac9ebb
Simon Pasquier <spasquier@mirantis.com>
2016-09-28 15:46:55 +0200

    Add the option to store the InfluxDB WAL in memory
    This change adds a new option to the Fuel UI to store the InfluxDB WAL
    files in memory. This greatly improves write performances but it may
    lead to data loss in case of server crash.
    
    DocImpact
    
    Change-Id: I6153cd24322ef3eab53505bf4cb1f72d9594ec04

--- deployment_scripts/puppet/manifests/influxdb.pp
+++ deployment_scripts/puppet/manifests/influxdb.pp
@@ -15,6 +15,7 @@
 notice('fuel-plugin-influxdb-grafana: influxdb.pp')
 
 $data_directory = hiera('lma::influxdb::data_dir')
+$wal_dir = "${data_directory}/wal"
 
 # We set raft_nodes only for the non-primary node. The primary node will be
 # started as the first node and it will be the leader of the Raft cluster.
@@ -37,11 +38,33 @@ file { $data_directory:
   require => User['influxdb'],
 }
 
+if hiera('lma::influxdb::wal::storage') == 'memory' {
+  $wal_size = hiera('lma::influxdb::wal::size')
+  file { $wal_dir:
+    ensure  => directory,
+    owner   => 'influxdb',
+    group   => 'influxdb',
+    require => File[$data_directory],
+  }
+
+  mount { $wal_dir:
+    ensure   => mounted,
+    device   => 'tmpfs',
+    atboot   => true,
+    options  => "size=${wal_size},rw",
+    fstype   => 'tmpfs',
+    remounts => false,
+    before   => Class['lma_monitoring_analytics::influxdb'],
+    require  => File[$wal_dir],
+  }
+}
+
 # We cannot mix IP addresses and hostnames otherwise the Raft cluster won't
 # start. We have to stick with IP addresses because hostnames map to the
 # managament network space.
 class { 'lma_monitoring_analytics::influxdb':
   base_directory => $data_directory,
+  wal_dir        => $wal_dir,
   hostname       => hiera('lma::influxdb::listen_address'),
   raft_nodes     => $raft_nodes,
   version        => '0.11.1-1',
